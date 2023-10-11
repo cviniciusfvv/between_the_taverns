cb17b935e6ef50a1002415067b3a4cac77915f34
Mantraz_ <127681851+cviniciusfvv@users.noreply.github.com>
2023-10-11 13:49:19 -0300

    1

--- exemplos-exercicios em aula/aula27-09/script.js
+++ exemplos-exercicios em aula/aula27-09/script.js
@@ -1,19 +1,15 @@
-function updateClock() {
-    const now = new Date();
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-    const second = now.getSeconds();
-  
-    const hourDeg = (hour % 12) * 30 + (minute / 60) * 30;
-    const minuteDeg = (minute / 60) * 360 + (second / 60) * 6;
-    const secondDeg = (second / 60) * 360;
-  
-    document.getElementById('hour').style.transform = `rotate(${hourDeg}deg)`;
-    document.getElementById('minute').style.transform = `rotate(${minuteDeg}deg)`;
-    document.getElementById('second').style.transform = `rotate(${secondDeg}deg)`;
-  
-    setTimeout(updateClock, 1000);
-  }
-  
-  updateClock();
-  
+function updateTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById('time').innerText = timeString;
+}
+
+// Atualize o tempo a cada segundo
+setInterval(updateTime, 1000);
+
+// Inicialize o tempo imediatamente
+updateTime();
